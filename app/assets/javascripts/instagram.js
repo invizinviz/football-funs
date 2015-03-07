094f373ada3a4622f0c0f6420650ad9a48e074d1
Andres Vara <andres.vara@gmail.com>
2015-03-07 11:14:41 +0200

    added ajax for tweets

--- app/assets/javascripts/instagram.js
+++ app/assets/javascripts/instagram.js
@@ -1,19 +1,46 @@
 $(function(){
 
   $('#toggle-official-instagram').on('click', function(){
-    // $('#instagram').hide();
     $('#twitter').slideToggle("slow");
     $('#instagram').slideToggle("slow");
 
   });
 
-  // $('').on('click', function(){
-  //   // $('#twitter').hide();
-  // });
-  
-
   $('#toggle-twitter').on('click', function(){
     $('#send-tweet').slideToggle("slow");
+  });
+
+
+  $('.new_tweet').on('submit', function(ev) {
+
+
+    var message = $('#tweet_message').val();
+        team_id = $('#tweet_team_id').val();
+
+    // console.log(message);
+  
+
+    var tweet = {
+                  tweet: {
+                    message: message,
+                    team_id: team_id
+                  }
+                };
+
+   console.log(tweet);
+    ev.preventDefault();
+
+
+    // var body = $('#tweet_message').val();
+    $.ajax({
+      url: '/tweets',
+      type: 'POST',
+      data: tweet,
+      success: function(data) {
+        console.log(tweet);
+        // $('#name).text(data.name);
+      }
+    });
   });
 
 
