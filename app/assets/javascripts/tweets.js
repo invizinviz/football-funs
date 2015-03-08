836e887b616eca2b3282427031ab8f858045e1a1
Andres Vara <andres.vara@gmail.com>
2015-03-08 16:39:44 +0000

    added count tweet stream

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,4 +1,8 @@
 $(function(){
+
+  var counter = 0;
+  var MAX_TWEETS_DISPLAYED = 20;
+
   var channel = $('body').data('channel');
 
 	var pusher = new Pusher('02abdbfcfa5036ae29ec');
@@ -8,16 +12,28 @@ $(function(){
 
     var html = '<li class="media "><div class="media-left"><a href="https://twitter.com/'+tweet.user.screen_name+'" target="_blank"><img src="' + tweet.user.profile_image_url + '" class="media-object"/></a></div><div class="media-body"><h4 class="media-heading"><a href="https://twitter.com/'+tweet.user.screen_name+'" target="_blank">'+ tweet.user.name+'</a></h4>' + tweet.text + '</div></li>';
 
-    $('#tweet-container').prepend(html);
+    $('#tweet-list').prepend(html);
 
-    $('#tweet-container').twitter_autolink();
+    $('#tweet-list').twitter_autolink();
 
     var $twittercounter = $('#toggle-twitter span');
 
     $twittercounter.text(Number($twittercounter.text())+1);
 
+    if(counter > MAX_TWEETS_DISPLAYED) {
+      $("#tweet-list li:last-child").remove();
+      // console.log(counter);
+    } else {
+        counter++;
+        // console.log("else: " + counter);
+    }
+    
+    // console.log(counter);
+
   });
 
   $('.team-timeline-tweets').twitter_autolink();
+
+
 
 });
